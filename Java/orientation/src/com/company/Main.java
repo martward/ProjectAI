dca979fe150a8c932522b5f67a51c153d915d4ea
Sebastien <sebastien@dutchnaoteam.nl>
2016-06-06 11:54:22 +0200

    Cleaned code

--- Java/orientation/src/com/company/Main.java
+++ Java/orientation/src/com/company/Main.java
@@ -3,37 +3,27 @@ package com.company;
 import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
-import java.nio.Buffer;
-import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) throws IOException{
 
-        ServerSocket listener = new ServerSocket(9090);
         System.out.println("Waiting for connection");
         System.out.println("blaaaaa");
-        try {
+        try (ServerSocket listener = new ServerSocket(9090)) {
             while (true) {
 
-                Socket socket = listener.accept();
-                try {
-                    //BufferedReader in =
-                    //      new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    //System.out.println("blablabla");
-                    //System.out.println(socket.getInputStream());
+                try (Socket socket = listener.accept()) {
+
                     DataInputStream in = new DataInputStream(socket.getInputStream());
 
                     System.out.println(in.readUTF());
 
-                } finally {
-                    socket.close();
                 }
+
+                if( listener.isClosed() )
+                    break;
             }
         }
-        finally {
-            listener.close();
-        }
     }
-
 }
