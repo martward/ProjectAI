f30a4626e73b298ac9d1948301d6348cee4a8436
Sebastien <sebastien@dutchnaoteam.nl>
2016-06-06 14:31:53 +0200

    Added framework for visualization of transform

---
+++ Java/orientation/src/com/company/IMUVisualizer.java
@@ -0,0 +1,73 @@
+package com.company;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class IMUVisualizer {
+
+    GUI theGUI;
+
+    public static void main(String[] args) throws IOException{
+
+        IMUVisualizer visualizer = new IMUVisualizer();
+
+    }
+
+    public IMUVisualizer()
+    {
+        theGUI = new GUI();
+
+        System.out.println("Opening socket");
+
+        handleConnection();
+    }
+
+    private void handleConnection()
+    {
+        try (ServerSocket listener = new ServerSocket(9090)) {
+            while (true) {
+                try (Socket socket = listener.accept()) {
+
+                    DataInputStream in = new DataInputStream(socket.getInputStream());
+
+                    String msg = in.readUTF();
+                    Transform transform = parseMessage(msg);
+
+                    theGUI.setRotationMatrix(transform);
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                if( listener.isClosed() )
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Transform parseMessage(String msg)
+    {
+        String[] arguments = msg.split("/");
+
+        Transform transform = new Transform(Double.parseDouble(arguments[1]),
+                Double.parseDouble(arguments[2]),
+                Double.parseDouble(arguments[3]) );
+
+        if (arguments[0].equals("relative"))
+        {
+            transform.mode = Transform.Mode.RELATIVE;
+        }
+        else if ( arguments[0].equals("absolute"))
+        {
+            transform.mode = Transform.Mode.ABSOLUTE;
+        }
+
+        System.out.println(transform);
+        return transform;
+    }
+
+
+}
