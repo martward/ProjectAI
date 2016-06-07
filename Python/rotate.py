89022b5918440f9328e21c686318491444dc1573
Joost Hoppenbrouwer <jpj.hoppenbrouwer@gmail.com>
2016-06-07 14:49:18 +0200

    added rotation visualization

--- Python/rotate.py
+++ Python/rotate.py
@@ -1,20 +1,31 @@
-import numpy as np
+from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 import math, time
+
+from matplotlib.path import Path
+import matplotlib.patches as patches
+
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 THETAS = np.radians([[10.,10.,10.], [-10.,35.,35.], [180.,-45.,90.]])
 
 def main():
-    normal = np.array([[0,0,1]])
-    xx, yy = np.meshgrid(range(6), range(6))
-    z = (-normal[0][0] * xx - normal[0][1] * yy) * 1. /normal[0][2]
+
+    fig = plt.figure(figsize = (22,15))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlim([-10,10])
+    ax.set_ylim([-10,10])
+    ax.set_zlim([-10,10])
     
-    plt3d = plt.figure().gca(projection='3d')
-    plt3d.plot_surface(xx, yy, z)
-    plt.draw()
-    
+    x = [-5,-5,5,5]
+    y = [-2.5,2.5,2.5,-2.5]
+    z = [0,0,0,0,0]
+    verts = [zip(x, y,z)]
+    surface = ax.add_collection3d(Poly3DCollection(verts))
+
     for i in range(3):
+
         THETA_X = THETAS[i][0]
         THETA_Y = THETAS[i][1]
         THETA_Z = THETAS[i][2]
@@ -23,12 +34,20 @@ def main():
         ROTY = np.array([[math.cos(THETA_Y),0,math.sin(THETA_Y)],[0,1,0],[-math.sin(THETA_Y),0,math.cos(THETA_Y)]])
         ROTZ = np.array([[math.cos(THETA_Z),-math.sin(THETA_Z),0],[math.sin(THETA_Z),math.cos(THETA_Z),0],[0,0,1]])
         ROT = ROTX.dot(ROTY.dot(ROTZ))
-        normal = normal.dot(ROT)
-        z = (-normal[0][0] * xx - normal[0][1] * yy) * 1. /normal[0][2]
+        
+        downLeft = np.asarray(verts[0][0]).dot(ROT)
+        upLeft = np.asarray(verts[0][1]).dot(ROT)
+        upRight = np.asarray(verts[0][2]).dot(ROT)
+        downRight = np.asarray(verts[0][3]).dot(ROT)
 
-        plt
-        plt3d.plot_surface(xx, yy, z)
-        plt.draw()
+        verts[0][0] = tuple(map(tuple, [downLeft]))[0]
+        verts[0][1] = tuple(map(tuple, [upLeft]))[0]
+        verts[0][2] = tuple(map(tuple, [upRight]))[0]
+        verts[0][3] = tuple(map(tuple, [downRight]))[0]
+        
+        ax.cla()
+        surface = ax.add_collection3d(Poly3DCollection(verts))
+        plt.pause(1)
         
 if __name__ == "__main__":
     main()
