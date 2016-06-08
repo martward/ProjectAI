76ab5cb66c5e32cf634e2cecf92dc844aafec0a6
Joost Hoppenbrouwer <jpj.hoppenbrouwer@gmail.com>
2016-06-08 20:52:04 +0200

    Setup base dependency

--- CardboardTranslation/app/src/main/java/ai/project/cardboardtranslation/MainActivity.java
+++ CardboardTranslation/app/src/main/java/ai/project/cardboardtranslation/MainActivity.java
@@ -2,6 +2,7 @@ package ai.project.cardboardtranslation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.vr.sdk.base.HeadTransform;
 
 public class MainActivity extends AppCompatActivity {
 
