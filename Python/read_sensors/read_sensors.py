6d38f737554a882fb0328f5f332a061d7b15c785
Joost Hoppenbrouwer <jpj.hoppenbrouwer@gmail.com>
2016-06-08 15:24:22 +0200

    Added ip shit again

--- Python/read_sensors/read_sensors.py
+++ Python/read_sensors/read_sensors.py
@@ -4,12 +4,8 @@ import numpy as np
 import math
 import thread
 from time import sleep
-
 from mpl_toolkits.mplot3d import Axes3D
 
-
-ip = "192.168.0.106"
-port = 9090
 
 class Visualizer:
 
@@ -26,21 +22,26 @@ class Visualizer:
     def handle_connection(self):
 
         try:
+            readIP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            readIP.connect(("8.8.8.8", 0))
+            ip = readIP.getsockname()[0]
+            readIP.close()
+
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             print "binding"
 
             try:
-                s.bind((ip,port))
+                s.bind((ip, 9090))
             except socket.error, msg:
                 print msg
             print "Waiting"
             s.listen(1)
-            c,addr = s.accept()
+            c, address = s.accept()
             print c
             print "Connected"
             while 1:
                 try:
-                    msg, addr = c.recvfrom(1024)
+                    msg, address = c.recvfrom(1024)
                     msg = msg[2:]
                     msg = msg.split("/")
 
@@ -75,10 +76,10 @@ class Visualizer:
 
         [xs, ys, zs] = self.rotatePoint(points, [0, 0, 0])
 
-        ax.scatter(xs, ys, zs, c=colors )
+        ax.scatter(xs, ys, zs, c=colors)
         fig.canvas.draw()
 
-        while(True):
+        while True:
             if len(self.queue) > 0:
 
                 msg = self.queue.pop()
@@ -93,7 +94,7 @@ class Visualizer:
                         ax.set_xlim([-10, 10])
                         ax.set_ylim([-10, 10])
                         ax.set_zlim([-10, 10])
-                        ax.scatter(xs, ys, zs, c=colors )
+                        ax.scatter(xs, ys, zs, c=colors)
                         fig.canvas.draw()
 
                     except ValueError:
@@ -104,42 +105,32 @@ class Visualizer:
 
                 elif msg[0] == "calibrate":
                     try:
+                        print "Calibrating"
                         self.calibratedPose[0, 0] = float(msg[3])
                         self.calibratedPose[0, 1] = float(msg[1])
                         self.calibratedPose[0, 2] = float(msg[2])
-
                         self.calibrated = True
-
-                        print "Calibrating"
-
                     except ValueError:
                         print repr(msg[1])
                         print repr(msg[2])
                         print repr(msg[3])
                         print "Non float value in message"
-
-
-
-
             else:
                 sleep(0.01)
-
         plt.show()
 
     def rotatePoint(self, points, theta):
         rads = np.radians(np.matrix(theta) - self.calibratedPose )
-        r = rads[0,0]
-        p = rads[0,1]
-        y = rads[0,2]
+        r = rads[0, 0]
+        p = rads[0, 1]
+        y = rads[0, 2]
         R = np.matrix([[math.cos(r)*math.cos(p), math.cos(r)*math.sin(p)*math.sin(y) - math.sin(r) * math.cos(y), math.cos(r)*math.sin(p)*math.cos(y) + math.sin(r)*math.sin(y)],
-                        [math.sin(r)*math.cos(p), math.cos(r)*math.sin(p)*math.sin(y) + math.cos(r) * math.cos(y), math.sin(r)*math.sin(p)*math.cos(y) - math.cos(r)*math.sin(y)],
-                        [-math.sin(p), math.cos(p)*math.sin(y), math.cos(p)*math.cos(y)]])
-
-        transformed = points*R
-        xs = np.squeeze(np.asarray( transformed[:, 0]))
-        ys = np.squeeze(np.asarray( transformed[:, 1]))
-        zs = np.squeeze(np.asarray( transformed[:, 2]))
-
+                       [math.sin(r)*math.cos(p), math.cos(r)*math.sin(p)*math.sin(y) + math.cos(r) * math.cos(y), math.sin(r)*math.sin(p)*math.cos(y) - math.cos(r)*math.sin(y)],
+                       [-math.sin(p), math.cos(p)*math.sin(y), math.cos(p)*math.cos(y)]])
+        transformed = points * R
+        xs = np.squeeze(np.asarray(transformed[:, 0]))
+        ys = np.squeeze(np.asarray(transformed[:, 1]))
+        zs = np.squeeze(np.asarray(transformed[:, 2]))
         return [xs, ys, zs]
 
 if __name__ == '__main__':
