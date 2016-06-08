9acbb9bac095046e000b88d35df5f13ac572eae0
Sebastien <sebastien@dutchnaoteam.nl>
2016-06-08 21:32:08 +0200

    Empty app with head tracking

--- CardboardTranslation/app/src/main/java/ai/project/cardboardtranslation/MainActivity.java
+++ CardboardTranslation/app/src/main/java/ai/project/cardboardtranslation/MainActivity.java
@@ -1,14 +1,73 @@
 package ai.project.cardboardtranslation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.google.vr.sdk.base.HeadTransform;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.vr.sdk.base.Eye;
+import com.google.vr.sdk.base.GvrActivity;
+import com.google.vr.sdk.base.GvrView;
+import com.google.vr.sdk.base.HeadTransform;
+import com.google.vr.sdk.base.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class MainActivity extends GvrActivity implements GvrView.StereoRenderer {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.ui_common);
+
+        GvrView gvrView = (GvrView)findViewById(R.id.gvr_view);
+
+        gvrView.setEGLConfigChooser(8, 8, 8, 8, 16, 8);
+
+        gvrView.setRenderer(this);
+        gvrView.setTransitionViewEnabled(true);
+
+        setGvrView(gvrView);
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+        String msg = "absolute/";
+        float[] rot = new float[4];
+
+        headTransform.getQuaternion(rot, 0);
+
+        msg = msg + rot[0] + "/" + rot[1] + "/" +rot[2] + "/" +rot[3];
+
+        log(msg);
+
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+
+    private void log(String s)
+    {
+        System.out.println(s);
     }
 }
