d0f0f18dd9a3fb44c51d33b25b02b30de198ee72
Joost Hoppenbrouwer <jpj.hoppenbrouwer@gmail.com>
2016-06-09 10:40:02 +0200

    Removed calibration stuff

--- Visualisation/Visualisation.py
+++ Visualisation/Visualisation.py
@@ -10,9 +10,6 @@ from mpl_toolkits.mplot3d import Axes3D
 class Visualizer:
 
     queue = []
-    calibrated = True
-
-    calibratedPose = np.matrix([0.0, 0.0, 0.0])
 
     def __init__(self):
         thread.start_new_thread(self.updateGUI, ())
@@ -25,18 +22,20 @@ class Visualizer:
             readIP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             readIP.connect(("8.8.8.8", 0))
             ip = readIP.getsockname()[0]
+            print ip
             readIP.close()
 
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             print "binding"
-
             try:
                 s.bind((ip, 9090))
             except socket.error, msg:
                 print msg
+
             print "Waiting"
             s.listen(1)
+
             c, address = s.accept()
             print c
             print "Connected"
@@ -46,13 +45,11 @@ class Visualizer:
                         msg, address = c.recvfrom(1024)
                         msg = msg[2:]
                         msg = msg.split("/")
-
                         if msg[0] == "stop":
                             print "Receiving messages stopped."
                             break
                         else:
-                            self.queue.insert(0,msg)
-
+                            self.queue.insert(0, msg)
                 except:
                     print "Connection Lost"
                     c.shutdown()
@@ -73,12 +70,10 @@ class Visualizer:
         points = np.matrix([[0, 2.5, 5], [0, 2.5, -5], [0, -2.5, -5], [0, -2.5, 5]])
 
         plt.ion()
-        fig = plt.figure()
-
+        fig = plt.figure(figsize=(22,15))
         ax = fig.add_subplot(111, projection='3d')
         ax.scatter([], [], [])
         colors = "black"
-
         ax.set_xlim([-10, 10])
         ax.set_ylim([-10, 10])
         ax.set_zlim([-10, 10])
@@ -90,48 +85,33 @@ class Visualizer:
 
         while True:
             if len(self.queue) > 0:
-
                 msg = self.queue.pop()
-                print msg
-                if msg[0] == "absolute" and self.calibrated:
-
+                if msg[0] == "absolute":
                     try:
-                        theta = [float(msg[1]), float(msg[2]), float(msg[3])]
-
-                        [xs, ys, zs] = self.rotatePoint(points, theta)
-
-                        plot._offsets3d = (xs,ys,zs)
+                        euler = [float(msg[1]), float(msg[2]), float(msg[3])]
+                        [xs, ys, zs] = self.rotatePoint(points, euler)
+                        plot._offsets3d = (xs, ys, zs)
                         fig.canvas.draw()
-
                     except ValueError:
                         print repr(msg[1])
                         print repr(msg[2])
                         print repr(msg[3])
                         print "Non float value in message"
-
-                elif msg[0] == "calibrate":
-                    try:
-                        print "Calibrating"
-                        self.calibratedPose[0, 0] = float(msg[1])
-                        self.calibratedPose[0, 1] = float(msg[2])
-                        self.calibratedPose[0, 2] = float(msg[3])
-                        self.calibrated = True
-                    except ValueError:
-                        print repr(msg[1])
-                        print repr(msg[2])
-                        print repr(msg[3])
-                        print "Non float value in message"
+                else:
+                    continue
             else:
                 sleep(0.01)
         plt.show()
 
-    def rotatePoint(self, points, theta):
-        rads = np.matrix(theta) - self.calibratedPose
+    def rotatePoint(self, points, euler):
+        rads = np.matrix(euler)
         r = rads[0, 0]
         p = rads[0, 1]
         y = rads[0, 2]
-        R = np.matrix([[math.cos(r)*math.cos(p), math.cos(r)*math.sin(p)*math.sin(y) - math.sin(r) * math.cos(y), math.cos(r)*math.sin(p)*math.cos(y) + math.sin(r)*math.sin(y)],
-                       [math.sin(r)*math.cos(p), math.cos(r)*math.sin(p)*math.sin(y) + math.cos(r) * math.cos(y), math.sin(r)*math.sin(p)*math.cos(y) - math.cos(r)*math.sin(y)],
+        R = np.matrix([[math.cos(r)*math.cos(p), math.cos(r)*math.sin(p)*math.sin(y) - math.sin(r) * math.cos(y),
+                        math.cos(r)*math.sin(p)*math.cos(y) + math.sin(r)*math.sin(y)],
+                       [math.sin(r)*math.cos(p), math.cos(r)*math.sin(p)*math.sin(y) + math.cos(r) * math.cos(y),
+                        math.sin(r)*math.sin(p)*math.cos(y) - math.cos(r)*math.sin(y)],
                        [-math.sin(p), math.cos(p)*math.sin(y), math.cos(p)*math.cos(y)]])
         transformed = points * R
         xs = np.squeeze(np.asarray(transformed[:, 0]))
