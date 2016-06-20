2a4d143c164fba94a65359098e0bf791a7fdacad
Sebastien <sebastien@dutchnaoteam.nl>
2016-06-20 15:32:01 +0200

    Removed old app

--- CardboardTranslation/app/src/main/java/ai/project/cardboardtranslation/CameraWrapper.java
+++
@@ -1,7 +0,0 @@
-package ai.project.cardboardtranslation;
-
-/**
- * Created by sebastien on 13-6-16.
- */
-public class CameraWrapper {
-}
--- CardboardTranslation/app/src/main/java/ai/project/cardboardtranslation/ImageRenderer.java
+++
@@ -1,7 +0,0 @@
-package ai.project.cardboardtranslation;
-
-/**
- * Created by Sébastien Negrijn on 13-6-16.
- */
-public class ImageRenderer {
-}
